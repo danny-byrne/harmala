c51641bac1489866f0f5bc97f8abf26be055f5a9
Danny Byrne <dannybyrne@DANIELs-MacBook-Pro.local>
2021-01-12 14:01:21 -0800

    added images

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { p1, p2, p3, p4, p5 } from "../constants/text.js";
+import img from "../images/harmala.jpeg";
 
 const blurb = [p1, p2, p3, p4].map((e) => {
   return <article>{e.padStart(2, " ")}</article>;
@@ -8,7 +9,7 @@ const blurb = [p1, p2, p3, p4].map((e) => {
 export default function About() {
   return (
     <>
-      <picture className="pictureBox" />
+      <img src={img} className="pictureBox" />
       <div className="blurb">{blurb}</div>
     </>
   );
